5a0ae47f17a0460b77bb9702582a4b27fd4f39e6
DhruvGupta1075 <96821313+DhruvGupta1075@users.noreply.github.com>
2025-11-13 16:58:27 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export function Navbar({ links = [], className }) {
         "backdrop-blur-md glass"
       )}>
         <Link to="/" className="flex items-center gap-2 md:gap-3 hover:opacity-90 focus:outline-none">
-          <img src={"/src/assets/logo1.png"} alt="HopeBridge Logo" className="w-8 h-8 md:w-10 md:h-10 rounded-full shadow-md" />
+          <img src={"/icon.png"} alt="HopeBridge Logo" className="w-8 h-8 md:w-10 md:h-10 rounded-full shadow-md" />
           <span className="text-lg md:text-2xl font-bold gradient-text">HopeBridge</span>
         </Link>
 
